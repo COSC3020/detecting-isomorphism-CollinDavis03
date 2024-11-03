ce3acf78d8cde889432d73d9b5ebe9e09015523d
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-11-03 14:20:27 -0700

    Update code.js

--- code.js
+++ code.js
@@ -103,10 +103,3 @@ function matrixEquality(m1, m2) {
 function arraysEqual(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]); 
 }
-
-
-
-module.exports = { are_isomorphic }; 
-
-
-            
